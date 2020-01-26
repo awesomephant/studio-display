a4da01b2172e284165a96c869ceeb8a83b5a7d09
Max Kohler <max@koehler-kn.de>
2020-01-26 20:37:23 +0000

    Switch to drive app for syncing

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ const app = require('express')();
 const http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const fs = require('fs')
-const { exec } = require('child_process');
-const downloadPath = './public/images/'
+const path = require('path');
+
+const downloadPath = './public/studio-display/'
 let imageList = []
 
 function updateImageList(cb) {
-    exec('node getFiles.js', (err, stdout, stderr) => {
-        if (err) {
-            console.error(error)
-        } else {
-            console.log(`${stdout}`);
-            fs.readdir(downloadPath, function (err, localFiles) {
-                imageList = localFiles
-                cb(imageList)
+    fs.readdir(downloadPath, function (err, localFiles) {
+        imageList = []
+        for (let i = 0; i < localFiles.length; i++){
+            let stats = fs.statSync(downloadPath + localFiles[i])
+            imageList.push({
+                filename: localFiles[i],
+                extension: path.extname(localFiles[i]),
+                lastModified: stats.mtime
             })
         }
+        cb(imageList)
     })
 }
 
@@ -25,6 +27,7 @@ function init(socket) {
     setInterval(function (socket) { updateImageList(socket) }, 10000)
 }
 app.use(express.static('public'))
+app.use(express.static('drive'))
 app.get('/', function (req, res) {
     res.sendFile(__dirname + 'index.html');
 });
@@ -33,11 +36,10 @@ http.listen(3000, function () {
 });
 io.on('connection', function (socket) {
     console.log('A user connected.');
-    io.emit('test')
     socket.on('getImageList', function (socket) {
-        console.log('got req')
+        console.log(new Date() + ': Building image list.')
         updateImageList(function (imageList) {
-            io.emit('imageList', imageList)
+            io.emit('imageList', imageList) 
         })
     })
 });
